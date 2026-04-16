405da50807abcba23b506711be579abbcccefce0
Teresa Fares <107094799+TeresaFares5@users.noreply.github.com>
2026-04-16 18:48:08 +1000

    Audio Update

--- Assets/TextMesh Pro/Examples & Extras/Scripts/TMP_TextSelector_B.cs
+++ Assets/TextMesh Pro/Examples & Extras/Scripts/TMP_TextSelector_B.cs
@@ -491,20 +491,20 @@ namespace TMPro.Examples
 
             // Restore UV0S
             // UVS0
-            Vector2[] src_uv0s = m_cachedMeshInfoVertexData[materialIndex].uvs0;
-            Vector2[] dst_uv0s = m_TextMeshPro.textInfo.meshInfo[materialIndex].uvs0;
-            dst_uv0s[vertexIndex + 0] = src_uv0s[vertexIndex + 0];
-            dst_uv0s[vertexIndex + 1] = src_uv0s[vertexIndex + 1];
-            dst_uv0s[vertexIndex + 2] = src_uv0s[vertexIndex + 2];
-            dst_uv0s[vertexIndex + 3] = src_uv0s[vertexIndex + 3];
+Vector4[] src_uv0s = m_cachedMeshInfoVertexData[materialIndex].uvs0;
+Vector4[] dst_uv0s = m_TextMeshPro.textInfo.meshInfo[materialIndex].uvs0;
+dst_uv0s[vertexIndex + 0] = src_uv0s[vertexIndex + 0];
+dst_uv0s[vertexIndex + 1] = src_uv0s[vertexIndex + 1];
+dst_uv0s[vertexIndex + 2] = src_uv0s[vertexIndex + 2];
+dst_uv0s[vertexIndex + 3] = src_uv0s[vertexIndex + 3];
 
-            // UVS2
-            Vector2[] src_uv2s = m_cachedMeshInfoVertexData[materialIndex].uvs2;
-            Vector2[] dst_uv2s = m_TextMeshPro.textInfo.meshInfo[materialIndex].uvs2;
-            dst_uv2s[vertexIndex + 0] = src_uv2s[vertexIndex + 0];
-            dst_uv2s[vertexIndex + 1] = src_uv2s[vertexIndex + 1];
-            dst_uv2s[vertexIndex + 2] = src_uv2s[vertexIndex + 2];
-            dst_uv2s[vertexIndex + 3] = src_uv2s[vertexIndex + 3];
+// UVS2
+Vector2[] src_uv2s = m_cachedMeshInfoVertexData[materialIndex].uvs2;
+Vector2[] dst_uv2s = m_TextMeshPro.textInfo.meshInfo[materialIndex].uvs2;
+dst_uv2s[vertexIndex + 0] = src_uv2s[vertexIndex + 0];
+dst_uv2s[vertexIndex + 1] = src_uv2s[vertexIndex + 1];
+dst_uv2s[vertexIndex + 2] = src_uv2s[vertexIndex + 2];
+dst_uv2s[vertexIndex + 3] = src_uv2s[vertexIndex + 3];
 
 
             // Restore last vertex attribute as we swapped it as well
@@ -525,12 +525,12 @@ namespace TMPro.Examples
             dst_colors[lastIndex + 3] = src_colors[lastIndex + 3];
 
             // UVS0
-            src_uv0s = m_cachedMeshInfoVertexData[materialIndex].uvs0;
-            dst_uv0s = m_TextMeshPro.textInfo.meshInfo[materialIndex].uvs0;
-            dst_uv0s[lastIndex + 0] = src_uv0s[lastIndex + 0];
-            dst_uv0s[lastIndex + 1] = src_uv0s[lastIndex + 1];
-            dst_uv0s[lastIndex + 2] = src_uv0s[lastIndex + 2];
-            dst_uv0s[lastIndex + 3] = src_uv0s[lastIndex + 3];
+src_uv0s = m_cachedMeshInfoVertexData[materialIndex].uvs0;
+dst_uv0s = m_TextMeshPro.textInfo.meshInfo[materialIndex].uvs0;
+dst_uv0s[lastIndex + 0] = src_uv0s[lastIndex + 0];
+dst_uv0s[lastIndex + 1] = src_uv0s[lastIndex + 1];
+dst_uv0s[lastIndex + 2] = src_uv0s[lastIndex + 2];
+dst_uv0s[lastIndex + 3] = src_uv0s[lastIndex + 3];
 
             // UVS2
             src_uv2s = m_cachedMeshInfoVertexData[materialIndex].uvs2;
